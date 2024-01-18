fcf7c4866b40e19c9497b99ce45b81f0d4fd78e2
Eleanor Wai <eleanor_wai@msn.com>
2024-01-18 03:20:16 +0000

    programming project chapter 3 random phonenumber

---
+++ randomphone.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class randomphone {
+
+
+    public static void main(String[] args){
+
+        /* Program that creates and prints a random phone number of the form xxx-xxx-xxxx
+        Include the dashes in the output. 
+        Do not let the first three digits contain 8 or 9 (but don't be more restrictive than that)
+        and make sure that the second set of three digits is not greater than 655
+        Hint: Think through the easiest way to construct the phone number
+        Each digit does not have to be determined separately.
+        */
+    }
+
+    String phonepart_1, phonepart_2, phonepart_3;
+    
+
+}
